68ff3c2aee7b75a200e77cbb5868c0ef959eae0e
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2019-12-22 11:33:40 +0100

    Added example usage of diffrent  dependency injection related to set run configuration

--- rTsd/rTsd/App.xaml.cs
+++ rTsd/rTsd/App.xaml.cs
@@ -11,7 +11,23 @@ namespace rTsd
             InitializeComponent();
 
             // Setup CDIs
-            DependencyService.Register<FeedService>();
+            // It follows an example on how to inject
+            // another service depenent on current set
+            // solution run configuration.
+            //
+            // It will use the MockedFeedService.cs if the
+            // DEBUG flag is set.
+            //
+            // Otherwise it will use the actual service with
+            // real life information and requests.
+            //
+            // For tutorial purpose, this functionality is not
+            // set active during the current implementation phase.
+//#if DEBUG
+            DependencyService.Register<MockedFeedService>();
+//#else
+            // DependencyService.Register<FeedService>();
+//#endif
 
             // Setup entry ui point.
             MainPage = new AppShell();
